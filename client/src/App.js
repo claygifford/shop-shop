aa472b9a06fada1838d66d5fbfc769fa8e6e200d
unknown <claygifford@gmail.com>
2022-09-05 12:23:19 -0400

    22.1.6: Refactor Components to Use Global State

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-
+import { StoreProvider } from "./utils/GlobalState";
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
@@ -40,15 +40,29 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/orderHistory" component={OrderHistory} />
-            <Route exact path="/products/:id" component={Detail} />
-            <Route component={NoMatch} />
-          </Switch>
+          <StoreProvider>
+            <Nav />
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/signup">
+                <Signup />
+              </Route>
+              <Route path="/orderHistory">
+                <OrderHistory />
+              </Route>
+              <Route path="/products/:id">
+                <Detail />
+              </Route>
+              <Route path="*">
+                <NoMatch />
+              </Route>
+            </Switch>
+          </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
